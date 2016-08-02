13b34f6b04aba294a7a6743a3cbe66dd54160864
PAPIDAKI <papidakis@gmail.com>
2016-08-02 19:58:15 +0300

    bf

--- db/migrate/20160802134820_rename_order_column_in_loading.rb
+++ db/migrate/20160802134820_rename_order_column_in_loading.rb
@@ -1,5 +1,7 @@
 class RenameOrderColumnInLoading < ActiveRecord::Migration
   def change
   	rename_column :loadings,:order,:ref
+  	remove_column :loadings,:ref
+  	add_column :loadings,:ref,:string
   end
 end
