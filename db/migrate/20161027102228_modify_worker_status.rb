0e3280e654985b0298af50a94c17b90bf0f61d94
PAPIDAKI <papidakis@gmail.com>
2017-07-30 20:02:48 +0300

    migration fixup

--- db/migrate/20161027102228_modify_worker_status.rb
+++ db/migrate/20161027102228_modify_worker_status.rb
@@ -1,10 +1,10 @@
 class ModifyWorkerStatus < ActiveRecord::Migration
   def up
   	# remove_column :workers,:status
-  	add_column :workers,:status,:integer
+  	# add_column :workers,:status,:integer
   end
 
   def down
-  	remove_column :workers,:status
+  	# remove_column :workers,:status
   end
 end
