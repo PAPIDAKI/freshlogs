977f01148380ef7ae14361c525b867f90693ab22
PAPIDAKI <papidakis@gmail.com>
2017-07-30 19:47:12 +0300

    old migration mistype firxed for heroku

--- db/migrate/20161023093423_change_worker_attributes.rb
+++ db/migrate/20161023093423_change_worker_attributes.rb
@@ -1,7 +1,7 @@
 class ChangeWorkerAttributes < ActiveRecord::Migration
   def up
   	# remove_column :workers,:active
-  	remove_column :workers,:startus
+  	remove_column :workers,:status
   	add_column :workers,:status,:string
   end
 end
