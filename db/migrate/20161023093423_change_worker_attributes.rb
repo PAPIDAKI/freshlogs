7e7b98100abcd6c8803a59223502a485c39d414e
PAPIDAKI <papidakis@gmail.com>
2017-07-30 19:58:52 +0300

    migration fixup

--- db/migrate/20161023093423_change_worker_attributes.rb
+++ db/migrate/20161023093423_change_worker_attributes.rb
@@ -2,6 +2,6 @@ class ChangeWorkerAttributes < ActiveRecord::Migration
   def up
   	# remove_column :workers,:active
   	# remove_column :workers,:status
-  	add_column :workers,:status,:string
+  	# add_column :workers,:status,:string
   end
 end
